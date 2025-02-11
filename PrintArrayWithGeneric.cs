744bf1777dbc1e749e057f4dc33773f6a604a7cf
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-11 17:05:17 +0530

    Print array using generics

---
+++ PrintArrayWithGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticePrablem
+{
+    internal class PrintArrayWithGeneric
+    {
+        public static void ToPrint<T>(T[] inputArr)
+        {
+            foreach (var element in inputArr)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using GenericPracticePrablem;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] inputStr = { "Hii", "i", "Asmita" };
+        int[] inputInt = { 0, 1, 2, 3, 4, 5, 6, 7 };
+        double[] inputDouble = { 0.1, 0.3, 4.5 };
+
+        PrintArrayWithGeneric.ToPrint(inputStr);
+        PrintArrayWithGeneric.ToPrint(inputInt);
+        PrintArrayWithGeneric.ToPrint(inputDouble);
+
+
+        
+    }
+}
