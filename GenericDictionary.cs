f34ea0e54d633cd8fc42b0419576c3cbcc83cef8
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-12 12:58:29 +0530

    generic collection type List and Dictionary

---
+++ GenericDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticePrablem
+{
+    internal class GenericDictionary
+    {
+        public void InsertInDictionary()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "pune");
+            dic.Add(2, "Mumbai");
+            dic.Add(3, "panvel");
+            dic.Add(4, "dhule");
+
+            foreach (KeyValuePair<int, string> pair in dic)
+            {
+                Console.WriteLine("key = " +pair.Key + ", " +"value = " + pair.Value);
+            }
+        }
+    }
+}
---
+++ GenericList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticePrablem
+{
+    internal class GenericList
+    {
+        public void DispayList()
+        {
+            List<String> list = new List<String>();
+
+            list.Add("Asmita");
+            list.Add("Chetan");
+            list.Add("we");
+            list.Add("are");
+            list.Add("Siblings");
+
+            foreach (String ele in list)
+            {
+                Console.WriteLine(ele);
+            }
+
+        }
+    }
+}
